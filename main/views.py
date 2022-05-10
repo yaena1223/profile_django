61d7ca53e75fe22b765496f63d166f28b874fd4b
yaena1223 <yaena1223@dgu.ac.kr>
2022-05-10 15:26:50 +0900

    0510 과제- CR추가

--- main/views.py
+++ main/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 def showmain(request):
-    return render(request,'main/mainpage.html')
+    posts = Post.objects.all()
+    return render(request,'main/mainpage.html', {'posts':posts})
+
+
+def detail(request,id):
+    post = get_object_or_404(Post, pk = id)
+    return render(request,'main/detail.html',{'post':post})
+
+def new(request):
+    return render(request, 'main/new.html')
+
+
+
+def create(request):
+    new_post = Post()
+    new_post.title = request.POST['title']
+    new_post.writer = request.POST['writer']
+    new_post.pub_date = timezone.now()
+    new_post.body = request.POST['body']
+    new_post.save()
+    return redirect('detail', new_post.id)
--- profile_page/urls.py
+++ profile_page/urls.py
@@ -20,4 +20,8 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.showmain, name = "showmain"),
+    path('<str:id>', views.detail, name = "detail"),
+    path('new/', views.new, name = "new"),
+    path('create/',views.create,name= "create"),
+
 ]
