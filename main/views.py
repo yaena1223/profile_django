562fe26f3084456f33c6770328d71409a9a223f1
yaena1223 <yaena1223@dgu.ac.kr>
2022-05-18 18:06:52 +0900

    0518 modify update

--- main/views.py
+++ main/views.py
@@ -36,7 +36,8 @@ def update(request,id):
     update_post.writer= request.POST['writer']
     update_post.pub_date = timezone.now()
     update_post.body = request.POST['body']
-    update_post.image = request.FILES.get('image')
+    if request.FILES.get("image"):
+        update_post.image = request.FILES.get('image')
     update_post.save()
     return redirect('main:detail', update_post.id)
 
