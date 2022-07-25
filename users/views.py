efcdf7f7988ac64798fba7b7b85d432267603e89
yaena1223 <yaena1223@dgu.ac.kr>
2022-07-26 05:10:16 +0900

    마이페이지 페이지네이션 적용

--- users/views.py
+++ users/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from requests import post
 from main.models import *
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator 
 # Create your views here.
 
 def mypage(request):
     user = request.user
-    posts = Post.objects.filter(writer=user)
+    post_list = Post.objects.filter(writer=user)
+    paginator = Paginator(post_list,8)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
     return render(request,'users/mypage.html',{'posts':posts})
 
 def mypage2(request):
     user = request.user
-    like_list = Like.objects.filter(user=user)
-    posts = Post.objects.filter(writer=user)
-    return render(request,'users/mypage2.html',{'posts':posts, 'like_list':like_list})
+    posts = Like.objects.filter(user=user)
+    paginator = Paginator(posts,8)
+    page = request.GET.get('page')
+    like_list = paginator.get_page(page)
+    return render(request,'users/mypage2.html',{'like_list':like_list})
